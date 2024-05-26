de809333d028bbc4af5a18c0627144e38c295609
Marco Germani <info@marcogermani.it>
2024-05-26 21:52:59 +0200

    Update fcm-notification.js

--- server/services/fcm-notification.js
+++ server/services/fcm-notification.js
@@ -15,7 +15,7 @@ const {
     transformPaginationResponse,
 } = require('@strapi/strapi');
 
-const { getFetchParams } = require('@strapi/strapi');
+const { getFetchParams } = require('../../../@strapi/strapi/dist/core-api/service/get-fetch-params');
 
 const {
     hasDraftAndPublish,
